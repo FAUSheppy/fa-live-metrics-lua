1058603630bb3bf4d41c302286065879e5eb9eae
Yannik Schmidt <yannik.schmidt@potaris.de>
2026-01-05 00:10:57 +0100

    fix: add icon, url & bump version

--- mod_info.lua
+++ mod_info.lua
@@ -1,13 +1,16 @@
 name = "FA_Metrics_Exporter"
-version = 1
+version = 2
 copyright = "GPLv3"
 description = [[Export live Metrics into the Gamelog
 Current considered in a beta state. Help is wellcome, contact me on FAF Discord.
 You add a Hotkey via F1 to toggel the mod, because it can lag the UI if your PC can't keep up.
 ]]
 author = "Sheppy"
-url = ""
-uid = "fa-metrics-exporter-01-sheppy"
+icon = "/mods/LiveMetrics/mod_icon.png"
+url = "https://github.com/FAUSheppy/fa-live-metrics-lua"
+uid = "fa-metrics-exporter-02-sheppy"
+conflicts = {
+    "fa-metrics-exporter-01-sheppy",
+}
 exclusive = false
 ui_only = true
-icon = ""
